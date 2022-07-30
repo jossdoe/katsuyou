3e841172a21bb5b7024d489c38e192714fff9a3e
Joss Doebler <joss@neuenarrative.de>
2022-07-30 22:34:17 +0200

    v1

--- components/layout.tsx
+++ components/layout.tsx
@@ -24,6 +24,24 @@ const Layout: React.FC<LayoutProps> = ({ children, active }) => {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <NavBar active={active} />
       <main className="w-full max-w-lg mx-auto px-8 py-8 mb-6">{children}</main>
